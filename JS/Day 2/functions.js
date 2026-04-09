3ff9d0afedab396d0e1f939269cc3c32f6620ca3
= <=>
2026-04-09 12:43:00 +0530

    updated js code

---
+++ JS/Day 2/functions.js
@@ -0,0 +1,50 @@
+
+
+let greet = function display(name){
+    console.log(name);
+}
+
+greet("abx");
+
+// IIFE
+(function(){
+    console.log("data fetched...");
+})()
+
+// Arrow Function
+let display = (name) => {
+    console.log("name is: " + name);
+} 
+
+display("ladooo");
+
+// Higher order function
+
+function calculator(a,b,operation){
+    return operation(a,b);
+}
+
+let add = (a,b) => {
+    return a+b;
+}
+
+let sub = (a,b) => {
+    return a-b;
+}
+
+let division = (a,b) => {
+    return a/b;
+}
+
+let remainder = (a,b) => {
+    return a%b;
+}
+
+let multiply = (a,b) => {
+    return a*b;
+}
+
+
+let result = calculator(10,20,multiply);
+console.log("Result is: " + result);
+
