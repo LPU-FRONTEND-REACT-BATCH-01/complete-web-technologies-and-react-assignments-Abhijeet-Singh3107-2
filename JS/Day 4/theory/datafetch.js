b44a2319e5f2d80701c5f2a2a5d52ed0c830706b
= <=>
2026-04-15 15:21:05 +0530

    fetching data

--- JS/Day 4/theory/datafetch.js
+++ JS/Day 4/theory/datafetch.js
@@ -3,8 +3,8 @@ let parent_container = document.querySelector(".parent-container");
 
 const fetchData = async () => {
     try{
-        // const data = await fetch("https://jsonplaceholder.typicode.com/todos/33");
-        const data = await fetch("http://localhost:8082/catalog/products");
+        const data = await fetch("https://jsonplaceholder.typicode.com/todos");
+        // const data = await fetch("http://localhost:8082/catalog/products");
         console.log(data);
         if(data.ok){
             const jsonData = await data.json();
